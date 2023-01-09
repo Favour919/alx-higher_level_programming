5620d79e96e092d3dab0deb1553f6ded693657f3
Favour Bassey <67082766+Favour919@users.noreply.github.com>
2023-01-09 15:51:05 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,4 +10,5 @@ class MyInt(int):
         return self.real != value
 
     def __ne__(self, value):
-        """Override != operator with == be
+        """Override != operator with == behavior"""
+        return self.real == value
